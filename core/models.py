499cec1c73ad9f7165e9349a8128b40e402701be
matinbhdrn77 <matinbhdrn77@gmail.com>
2022-07-31 10:40:52 +0430

    add test for creating user

--- core/models.py
+++ core/models.py
@@ -1,3 +1,8 @@
+"""
+Database models.
+"""
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
-# Create your models here.
+
+class User(AbstractBaseUser, PermissionsMixin)
