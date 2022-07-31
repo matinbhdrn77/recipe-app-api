11aaa781190d03821309ec9c78ea828de1e6357c
matinbhdrn77 <matinbhdrn77@gmail.com>
2022-07-31 11:13:23 +0430

    create user model from abstracr classes

--- core/models.py
+++ core/models.py
@@ -1,8 +1,30 @@
 """
 Database models.
 """
+import email
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 
-class User(AbstractBaseUser, PermissionsMixin)
+class UserManager(BaseUserManager):
+    """Manager for users."""
+
+    def create_user(self, email, password, **kwargs):
+        """Create, Save and return a new user."""
+        user = self.model(email=email, **kwargs)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
