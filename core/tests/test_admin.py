d12aa9fefd5d1700ed22732de132d370278e2b61
matinbhdrn77 <matinbhdrn77@gmail.com>
2022-08-01 18:58:44 +0430

    Create test for admin user list

---
+++ core/tests/test_admin.py
@@ -0,0 +1,32 @@
+"""
+Test for Django admin modidfication
+"""
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+    """Test for Django admin."""
+
+    def setUp(self):
+        """Create user and clien"""
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@ex.com',
+            password='test123',
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='ueser@ex.com',
+            password='pass123',
+            name='Test User'
+        )
+     
+    def test_user_list(self):
+        """Test that users are listed on page."""
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertEqual(res, self.user.email)
