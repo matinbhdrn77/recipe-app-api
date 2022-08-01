3fd3c7e422b9386001db6bd91a6d815df4f4bbdd
matinbhdrn77 <matinbhdrn77@gmail.com>
2022-08-01 18:24:27 +0430

    add normailized email test and it function to model

--- core/models.py
+++ core/models.py
@@ -10,6 +10,8 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password, **kwargs):
         """Create, Save and return a new user."""
+        if not email:
+            raise ValueError("User must have email address")
         user = self.model(email=self.normalize_email(email), **kwargs)
         user.set_password(password)
         user.save(using=self._db)
