b7920f8a8a56eea7337b6aedca03f0610e953be4
matinbhdrn77 <matinbhdrn77@gmail.com>
2022-08-02 19:52:35 +0430

    connect urls for user app and views

--- user/urls.py
+++ user/urls.py
@@ -2,3 +2,10 @@
 Urls for handling user registration and login
 """
 from django.urls import path, include
+
+from .views import CreateUserView
+
+app_name = 'user'
+urlpatterns = [
+    path('create/', CreateUserView.as_view(), name='create'),
+]
