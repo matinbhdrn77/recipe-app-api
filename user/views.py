834f24d52848f6d305cf22b481f9bf9c584b5cb9
matinbhdrn77 <matinbhdrn77@gmail.com>
2022-08-03 13:11:56 +0430

    create a view for handling new token

--- user/views.py
+++ user/views.py
@@ -2,10 +2,18 @@
 API class base view for handling user features
 """
 from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
-from .serializers import UserSerializer
+from .serializers import UserSerializer, AuthTokenSerializer
+
 
 class CreateUserView(generics.CreateAPIView):
     """"Create a new user in the system"""
     serializer_class = UserSerializer
-    
+
+
+class CreateTokenView(ObtainAuthToken):
+    """Create a new auth token for use."""
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
