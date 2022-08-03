e5d321942aecb43f9ef106e044bcea91d48bf838
matinbhdrn77 <matinbhdrn77@gmail.com>
2022-08-03 13:11:07 +0430

    configure token url and setting

--- user/urls.py
+++ user/urls.py
@@ -3,9 +3,10 @@ Urls for handling user registration and login
 """
 from django.urls import path, include
 
-from .views import CreateUserView
+from .views import CreateUserView, CreateTokenView
 
 app_name = 'user'
 urlpatterns = [
     path('create/', CreateUserView.as_view(), name='create'),
+    path('token/', CreateTokenView.as_view(), name='token'),
 ]
