e8fa7e880c1269f2497c59b94be776330a7b1ba9
matinbhdrn77 <matinbhdrn77@gmail.com>
2022-08-09 10:51:53 +0430

    create user manage view

--- user/urls.py
+++ user/urls.py
@@ -3,10 +3,11 @@ Urls for handling user registration and login
 """
 from django.urls import path, include
 
-from .views import CreateUserView, CreateTokenView
+from .views import CreateUserView, CreateTokenView, ManageUserView
 
 app_name = 'user'
 urlpatterns = [
     path('create/', CreateUserView.as_view(), name='create'),
     path('token/', CreateTokenView.as_view(), name='token'),
+    path('me/', ManageUserView.as_view(), name='me'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,7 +1,7 @@
 """
 API class base view for handling user features
 """
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
@@ -17,3 +17,13 @@ class CreateTokenView(ObtainAuthToken):
     """Create a new auth token for use."""
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """Retieve and return the authenticated user."""
+        return self.request.user
