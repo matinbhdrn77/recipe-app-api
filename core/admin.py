42c748af4f674c5afab16f2006a7ec1dd36bc89d
matinbhdrn77 <matinbhdrn77@gmail.com>
2022-08-11 10:31:36 +0430

    add recipe app to project and admin panel

--- core/admin.py
+++ core/admin.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 
-from core.models import User
+from core.models import User, Recipe
 
 
 @admin.register(User)
@@ -30,3 +30,5 @@ class UserAdmin(BaseUserAdmin):
             'fields': ('email', 'password1', 'password2', 'name', 'is_active', 'is_staff', 'is_superuser'),
         }),
     )
+
+admin.site.register(Recipe)
