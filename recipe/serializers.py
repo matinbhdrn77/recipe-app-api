e4ab374e8ea646525246099cbf24e1c2da8fb5dc
matinbhdrn77 <matinbhdrn77@gmail.com>
2022-08-13 11:32:47 +0430

    create a detail serializer for recipe and add it to our view set

--- recipe/serializers.py
+++ recipe/serializers.py
@@ -1,6 +1,7 @@
 """
 Serializers for recipe APIs
 """
+from pyexpat import model
 from rest_framework import serializers
 
 from core.models import Recipe
@@ -13,3 +14,10 @@ class RecipeSerializer(serializers.ModelSerializer):
         model = Recipe
         fields = ['id', 'title', 'time_minutes', 'price', 'link']
         read_only_fields = ['id']
+
+
+class RecipeDetailSerializer(serializers.ModelSerializer):
+    """Serializer for single recipe."""
+
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ['description']
