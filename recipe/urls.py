271a46c94def7c4053c0592bac29ef03d7e664cf
matinbhdrn77 <matinbhdrn77@gmail.com>
2022-08-15 18:16:08 +0430

    Create tag viewset and add it to router

--- recipe/urls.py
+++ recipe/urls.py
@@ -5,11 +5,12 @@ from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
 
-from recipe.views import RecipeViewSet
+from recipe.views import RecipeViewSet, TagViewSet
 
 
 router = DefaultRouter()
 router.register('recipes', RecipeViewSet)
+router.register('tags', TagViewSet)
 
 app_name = 'recipe'
 
