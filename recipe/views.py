49d0d37ccf60b8ad586fd2f7193a1507bb7bd6e8
matinbhdrn77 <matinbhdrn77@gmail.com>
2022-09-11 11:51:47 +0430

    format code

--- recipe/views.py
+++ recipe/views.py
@@ -9,7 +9,6 @@ from rest_framework.permissions import IsAuthenticated
 
 from core.models import Ingredient, Recipe, Tag
 from recipe import serializers
-import recipe
 
 
 class RecipeViewSet(viewsets.ModelViewSet):
@@ -33,18 +32,19 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
-    
+
     @action(methods=['POST'], detail=True, url_path='upload-image')
     def upload_image(self, request, pk=None):
         """Upload an image to a recipe."""
         recipe = self.get_object()
-        serializer = self.get_serializer(recipe, data=request.data)  
+        serializer = self.get_serializer(recipe, data=request.data)
 
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
-        
+
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class BaseRecipeAttrViewSet(mixins.ListModelMixin,
                             mixins.DestroyModelMixin,
@@ -57,7 +57,6 @@ class BaseRecipeAttrViewSet(mixins.ListModelMixin,
     def get_queryset(self):
         """Filter queryset to authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by('-name')
-
 
 
 class TagViewSet(BaseRecipeAttrViewSet):
